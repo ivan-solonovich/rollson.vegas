af978beaad9ff2e40d0dfc0ae554bc1ee1b9c604
ivan-solonovich <solonovichivan1978@gmail.com>
2025-01-20 20:09:07 +0300

    first commit

--- components/mobile/servises-mobile/services-mobile-wrapper.vue
+++ components/mobile/servises-mobile/services-mobile-wrapper.vue
@@ -6,7 +6,7 @@
   <div class="w-100 grid grid-cols-1">
     <div class="w-100 h-100 flex flex-col justify-center align-middle">
       <hr class="w-2/3 h-2 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
-      <h1 class="services-title p-4 text-5xl text-center">Our services</h1>
+      <h1 class="services-title p-4 text-5xl text-center">Premium Ceiling and lighting</h1>
       <hr class="w-2/3 h-2 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
   </div>
     <!--  section-3-->
