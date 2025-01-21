c4695337ae9de1e442817728990db0dd32f23646
ivan-solonovich <solonovichivan1978@gmail.com>
2025-01-21 18:53:16 +0300

    first commit

--- pages/hoveringlines.vue
+++ pages/hoveringlines.vue
@@ -21,6 +21,9 @@ const { isTablet } = useDevice();
           <h1 class="services-title text-white p-8 text-7xl text-center uppercase">Light Lines</h1>
           <hr class="w-2/3 h-2 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
         </div>
+        <div class="bathroom-img w-100 h-auto flex justify-center align-middle pt-4">
+          <img class="image-light" loading="lazy" src="@/assets/images/Light_lines/01.webp" alt="">
+        </div>
         <div class="about-wrapper grid grid-cols-1 gap-8">
           <div class="about-article text-white flex flex-col p-8 m-8">
             <h1 class="title-article-about text-5xl text-center"> Light Lines for stretch ceilings</h1>
@@ -170,8 +173,10 @@ const { isTablet } = useDevice();
           <hr class="w-2/3 h-2 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
         </div>
       </div>
+      <div class="bathroom-img w-100 h-auto flex justify-center align-middle pt-4">
+        <img class="image-light" loading="lazy" src="@/assets/images/Light_lines/01.webp" alt="">
+      </div>
       <div class="about-wrapper grid grid-cols-1 gap-8">
-
         <div class="about-article text-white flex flex-col p-8">
           <h1 class="title-article-about text-5xl text-center"> Light Lines for stretch ceilings</h1>
           <div class="left-0">
